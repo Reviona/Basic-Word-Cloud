4689d44863db879243cae5c96cbd45623982c7df
Reviona <32984956+Reviona@users.noreply.github.com>
2022-03-25 20:40:25 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import jieba
 import wordcloud
+import time
 
 def stopwordslist(filepath):    # 读取停用词表
     stopwords = [line.strip() for line in open(filepath, 'r', encoding='utf-8').readlines()]
     return stopwords
 
-with open("test.txt", encoding="utf-8") as f:   #打开文件，可以使用相对路径或绝对路径
+filepath = input("请输入绝对或相对路径：")
+time_start = time.time()    # 运行时间计时开始
+with open(filepath, encoding="utf-8") as f:   #打开文件，可以使用相对路径或绝对路径
     s = f.read()
 ls = jieba.lcut(s)  # 分词列表
 text = ' '.join(ls)  # 连接字符串
@@ -23,3 +26,6 @@ wc = wordcloud.WordCloud(
 
 wc.generate(text)  # 加载文本
 wc.to_file("result.png")  # 输出词云图片
+time_end = time.time()  # 运行时间计时结束
+time_cost = time_end - time_start
+print("计算完成，图片已保存在工作目录下，共耗时" + str("%.3f" % time_cost) + "s.")
